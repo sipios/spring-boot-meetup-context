3b45c42f26365706e48c32f8aeb4ad62fd36c666
SamNal2008 <samyn@sipios.com>
2024-03-11 00:38:16 +0100

    feat: create a random useless service

---
+++ src/main/java/fr/sipios/springmeetup/infrastructure/RandomService.java
@@ -0,0 +1,14 @@
+package fr.sipios.springmeetup.infrastructure;
+
+import static fr.sipios.springmeetup.utils.SleepUtils.sleep;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class RandomService {
+  public RandomService() {
+    log.info("RandomService initialized ans sleeping for 5 seconds");
+    sleep(5);
+  }
+}
