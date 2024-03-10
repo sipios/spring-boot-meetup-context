1b00f1f87067ad98011b2317a8c773d8b7827e0d
SamNal2008 <samyn@sipios.com>
2024-03-11 00:02:37 +0100

    feat: initialize spring boot app with test container

---
+++ src/main/java/fr/sipios/springmeetup/SpringMeetupApplication.java
@@ -0,0 +1,13 @@
+package fr.sipios.springmeetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMeetupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMeetupApplication.class, args);
+    }
+
+}
